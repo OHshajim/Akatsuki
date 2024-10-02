db82ecbd0bf6f4d23a2adf33bceb6b5a8f2a64b2
OHshajim <ajshajimmax@gmail.com>
2024-10-02 23:37:33 +0600

    team add

---
+++ src/Components/Home/Team.tsx
@@ -0,0 +1,14 @@
+import SectionTitle from "@/Shared/SectionTitle";
+
+const Team = () => {
+  return (
+    <div>
+      <SectionTitle
+        subHeading={"Our Team"}
+        heading={"Meet with Us"}
+      />
+    </div>
+  );
+};
+
+export default Team;
