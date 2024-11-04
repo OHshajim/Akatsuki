df5b4ab232c38f4db54f75c13a76a45b46187651
OHshajim <ajshajimmax@gmail.com>
2024-11-04 23:20:02 +0600

    data update

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,7 +1,7 @@
 import SectionBanner from "@/Shared/SectionBanner";
-import { popular } from "../../../Public/Popular";
 import Image from "next/image";
 import { Rating } from "@smastrom/react-rating";
+import { movies } from "../../../Public/Shop";
 
 const page = () => {
   return (
@@ -9,22 +9,22 @@ const page = () => {
       <div className="bg-white">
         <SectionBanner subTitle={"Home > Movies"} title={"Movies"} />
         <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-10 container mx-auto py-20">
-          {popular.map((movie) => (
+          {movies.map((movie) => (
             <div
-              key={movie.product_id}
+              key={movie.title}
               className="card rounded-none text-black"
             >
               <figure className=" relative group">
                 <Image
                   width={600}
                   height={800}
-                  src={movie.image}
+                  src={movie.imageUrl[0]}
                   alt="Shoes"
                   className="w-full group"
                 />
                 <div className="absolute bottom-0 w-full group-hover:flex hidden bg-black group-hover:duration-200 group-hover:delay-100">
                   <button className="btn bg-black w-1/2 border-none hover:bg-gray-800 text-white font-bold">
-                  Add to Cart
+                    Add to Cart
                   </button>
                   <button className="btn bg-black w-1/2 border-none hover:bg-gray-800 text-white font-bold">
                     Add to Wishlist
@@ -33,11 +33,11 @@ const page = () => {
               </figure>
               <div className="card-body py-2 px-1">
                 <h2 className="card-title text-black bebas-neue tracking-[2px] font-medium">
-                  {movie.name}
+                  {movie.title}
                 </h2>
                 <div className="flex justify-between">
                   <h3 className="font-semibold text-zinc-400">
-                    {movie.tags[1]}
+                    {movie.genres[1]}
                   </h3>
                   <Rating
                     style={{ maxWidth: 110, color: "#fff" }}
