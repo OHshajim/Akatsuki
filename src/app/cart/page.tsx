ca61e93756129c8f885568678328bf17362be644
OHshajim <ajshajimmax@gmail.com>
2024-12-04 01:13:32 +0600

    BUILD file update

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -147,198 +147,202 @@ const Cart = () => {
           )
         )}
       </div>
-      {currentStep === 1 && cartItems?.length > 0 ? (
-        <div>
-          {/* Cart Table */}
-          <section className="container mx-auto px-4 py-10">
+      {currentStep === 1 && (
+        <>
+          {cartItems?.length > 0 ? (
             <div>
-              <div className=" hidden lg:block">
-                <table className="w-full bg-white rounded-lg shadow-lg">
-                  <thead className="bg-gray-100">
-                    <tr>
-                      <th className="py-4 px-6 text-left">Product</th>
-                      <th className="py-4 px-6 text-left">Price</th>
-                      <th className="py-4 px-6 text-left">Quantity</th>
-                      <th className="py-4 px-6 text-left">Subtotal</th>
-                      <th className="py-4 px-6 text-left">Remove</th>
-                    </tr>
-                  </thead>
-                  {cartItems.map((item) => (
-                    <CartsDetails
-                      setRemoveId={setRemoveId}
-                      item={item}
-                      key={item._id}
-                      handleQuantityChange={handleQuantityChange}
-                    />
-                  ))}
-                </table>
-              </div>
-              {/* Card style for small screens */}
-              <div className="block lg:hidden space-y-4">
-                <h3 className="font-primary text-xl">Products</h3>
-                {cartItems.map((cartItem) => (
-                  <div
-                    key={cartItem._id}
-                    className="relative flex flex-col items-start p-4 bg-gray-100 rounded-lg shadow-sm"
-                  >
-                    {/* Remove button */}
-                    <button
-                      className="absolute top-2 right-2 text-gray-500 hover:text-rose-500"
-                      aria-label="Remove"
-                    >
-                      &times;
-                    </button>
-                    {/* Product Image */}
-                    <div className="w-full flex items-center gap-4">
-                      <Image
-                        width={120}
-                        height={120}
-                        className="object-cover rounded p-2"
-                        src={cartItem.imageUrl}
-                        alt={cartItem.title}
-                      />
-                      <div>
-                        <h2 className=" text-black font-primary text-lg">
-                          {cartItem.title}
-                        </h2>
-                        <p className="text-sm text-gray-800 font-medium">
-                          By {cartItem.author}
-                        </p>
-                      </div>
-                    </div>
-                    {/* Product Details */}
-                    <div className="w-full mt-4">
-                      <p className="text-sm text-gray-700 flex justify-between">
-                        <span className="font-bold">Price : </span> $
-                        {cartItem.price.toFixed(2)}
-                      </p>
-                      <p className="text-sm text-gray-700 flex justify-between">
-                        <span className="font-bold">Quantity : </span>{" "}
-                        <input
-                          type="number"
-                          min="1"
-                          defaultValue={1}
-                          value={cartItem.quantity}
-                          onChange={(e) =>
-                            handleQuantityChange(
-                              cartItem._id,
-                              parseInt(e.target.value)
-                            )
-                          }
-                          className="w-12 border border-gray-300 rounded-none text-center py-2"
+              {/* Cart Table */}
+              <section className="container mx-auto px-4 py-10">
+                <div>
+                  <div className=" hidden lg:block">
+                    <table className="w-full bg-white rounded-lg shadow-lg">
+                      <thead className="bg-gray-100">
+                        <tr>
+                          <th className="py-4 px-6 text-left">Product</th>
+                          <th className="py-4 px-6 text-left">Price</th>
+                          <th className="py-4 px-6 text-left">Quantity</th>
+                          <th className="py-4 px-6 text-left">Subtotal</th>
+                          <th className="py-4 px-6 text-left">Remove</th>
+                        </tr>
+                      </thead>
+                      {cartItems.map((item) => (
+                        <CartsDetails
+                          setRemoveId={setRemoveId}
+                          item={item}
+                          key={item._id}
+                          handleQuantityChange={handleQuantityChange}
                         />
-                      </p>
-                      <p className="text-sm text-gray-700 flex justify-between">
-                        <span className="font-bold">Subtotal : </span> $
-                        {cartItem.quantity
-                          ? (cartItem.price * cartItem.quantity).toFixed(2)
-                          : cartItem.price}
-                      </p>
-                    </div>
+                      ))}
+                    </table>
                   </div>
-                ))}
-              </div>
-            </div>
-
-            {/* Buttons */}
-            <div className="flex justify-end items-center mt-6 gap-5">
-              <Link
-                href="/shop"
-                className="btn px-6 py-2 border-none rounded-none bg-white text-black shadow-md  tracking-widest font-normal"
-              >
-                Continue Shopping
-              </Link>
-            </div>
-          </section>
-
-          {/* Cart Totals */}
-          <section className="container mx-auto px-4 py-10">
-            <h2 className="text-xl font-bold mb-4">Cart Totals</h2>
-            <div className="bg-white rounded-lg shadow-lg p-6">
-              <div className="flex justify-between border-b pb-4">
-                <span>Subtotal</span>
-                <span>${subtotal?.toFixed(2)}</span>
-              </div>
-              <div className="flex justify-between border-b py-4">
-                <span>Shipping</span>
-                <span>$10</span>
-              </div>
-              <div className="flex justify-between font-bold text-lg mt-4">
-                <span>Total</span>
-                <span>${(subtotal + 10)?.toFixed(2)}</span>
-              </div>
-
-              {/* Address Form */}
-              {isAddressFormVisible ? (
-                <form onSubmit={handleSubmit(onSubmit)} className="mt-6">
-                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    <input
-                      {...register("country", { required: true })}
-                      placeholder="Country"
-                      className="p-2 border rounded-md"
-                    />
-                    <input
-                      {...register("state", { required: true })}
-                      placeholder="State"
-                      className="p-2 border rounded-md"
-                    />
-                    <input
-                      {...register("city", { required: true })}
-                      placeholder="City"
-                      className="p-2 border rounded-md"
-                    />
-                    <input
-                      {...register("zip", { required: true })}
-                      placeholder="ZIP Code"
-                      className="p-2 border rounded-md"
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="mt-4 px-6 py-2 bg-[#6fc9cd] hover:bg-[#2a9ca2]  text-white rounded-md shadow-md"
-                  >
-                    Update Address
-                  </button>
-                </form>
-              ) : (
-                <div className="mt-6">
-                  <div className="flex justify-between">
-                    <p className="text-gray-700">Address:</p>
-                    <div>
-                      <p className="text-gray-700">
-                        {address?.city}, {address?.state}, {address?.country} -{" "}
-                        {address?.zip}
-                      </p>
-                      <button
-                        onClick={handleAddressEdit}
-                        className="mt-2 text-[#6fc9cd] hover:text-[#2a9ca2] text-sm hover:underline text-end w-full"
+                  {/* Card style for small screens */}
+                  <div className="block lg:hidden space-y-4">
+                    <h3 className="font-primary text-xl">Products</h3>
+                    {cartItems.map((cartItem) => (
+                      <div
+                        key={cartItem._id}
+                        className="relative flex flex-col items-start p-4 bg-gray-100 rounded-lg shadow-sm"
                       >
-                        Change Location?
-                      </button>
-                    </div>
+                        {/* Remove button */}
+                        <button
+                          className="absolute top-2 right-2 text-gray-500 hover:text-rose-500"
+                          aria-label="Remove"
+                        >
+                          &times;
+                        </button>
+                        {/* Product Image */}
+                        <div className="w-full flex items-center gap-4">
+                          <Image
+                            width={120}
+                            height={120}
+                            className="object-cover rounded p-2"
+                            src={cartItem.imageUrl}
+                            alt={cartItem.title}
+                          />
+                          <div>
+                            <h2 className=" text-black font-primary text-lg">
+                              {cartItem.title}
+                            </h2>
+                            <p className="text-sm text-gray-800 font-medium">
+                              By {cartItem.author}
+                            </p>
+                          </div>
+                        </div>
+                        {/* Product Details */}
+                        <div className="w-full mt-4">
+                          <p className="text-sm text-gray-700 flex justify-between">
+                            <span className="font-bold">Price : </span> $
+                            {cartItem.price.toFixed(2)}
+                          </p>
+                          <p className="text-sm text-gray-700 flex justify-between">
+                            <span className="font-bold">Quantity : </span>{" "}
+                            <input
+                              type="number"
+                              min="1"
+                              defaultValue={1}
+                              value={cartItem.quantity}
+                              onChange={(e) =>
+                                handleQuantityChange(
+                                  cartItem._id,
+                                  parseInt(e.target.value)
+                                )
+                              }
+                              className="w-12 border border-gray-300 rounded-none text-center py-2"
+                            />
+                          </p>
+                          <p className="text-sm text-gray-700 flex justify-between">
+                            <span className="font-bold">Subtotal : </span> $
+                            {cartItem.quantity
+                              ? (cartItem.price * cartItem.quantity).toFixed(2)
+                              : cartItem.price}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
-              )}
-            </div>
 
-            {/* Checkout Button */}
-            <button
-              onClick={handleCheckout}
-              className="btn  tracking-widest font-medium  mt-6 w-full px-6 py-3 bg-[#6fc9cd] hover:bg-[#2a9ca2] text-white  rounded-none"
-            >
-              Proceed to Checkout
-            </button>
-          </section>
-        </div>
-      ) : (
-        <div className="flex justify-center items-center flex-col">
-          <h4 className=" text-2xl font-semibold">No Cart Available !!!</h4>
-          <Link href="/shop">
-            <button className="btn px-6 py-2 border-none rounded-none bg-white text-black shadow-md  tracking-widest font-normal mt-4">
-              Continue Shopping
-            </button>
-          </Link>
-        </div>
+                {/* Buttons */}
+                <div className="flex justify-end items-center mt-6 gap-5">
+                  <Link
+                    href="/shop"
+                    className="btn px-6 py-2 border-none rounded-none bg-white text-black shadow-md  tracking-widest font-normal"
+                  >
+                    Continue Shopping
+                  </Link>
+                </div>
+              </section>
+
+              {/* Cart Totals */}
+              <section className="container mx-auto px-4 py-10">
+                <h2 className="text-xl font-bold mb-4">Cart Totals</h2>
+                <div className="bg-white rounded-lg shadow-lg p-6">
+                  <div className="flex justify-between border-b pb-4">
+                    <span>Subtotal</span>
+                    <span>${subtotal?.toFixed(2)}</span>
+                  </div>
+                  <div className="flex justify-between border-b py-4">
+                    <span>Shipping</span>
+                    <span>$10</span>
+                  </div>
+                  <div className="flex justify-between font-bold text-lg mt-4">
+                    <span>Total</span>
+                    <span>${(subtotal + 10)?.toFixed(2)}</span>
+                  </div>
+
+                  {/* Address Form */}
+                  {isAddressFormVisible ? (
+                    <form onSubmit={handleSubmit(onSubmit)} className="mt-6">
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <input
+                          {...register("country", { required: true })}
+                          placeholder="Country"
+                          className="p-2 border rounded-md"
+                        />
+                        <input
+                          {...register("state", { required: true })}
+                          placeholder="State"
+                          className="p-2 border rounded-md"
+                        />
+                        <input
+                          {...register("city", { required: true })}
+                          placeholder="City"
+                          className="p-2 border rounded-md"
+                        />
+                        <input
+                          {...register("zip", { required: true })}
+                          placeholder="ZIP Code"
+                          className="p-2 border rounded-md"
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        className="mt-4 px-6 py-2 bg-[#6fc9cd] hover:bg-[#2a9ca2]  text-white rounded-md shadow-md"
+                      >
+                        Update Address
+                      </button>
+                    </form>
+                  ) : (
+                    <div className="mt-6">
+                      <div className="flex justify-between">
+                        <p className="text-gray-700">Address:</p>
+                        <div>
+                          <p className="text-gray-700">
+                            {address?.city}, {address?.state},{" "}
+                            {address?.country} - {address?.zip}
+                          </p>
+                          <button
+                            onClick={handleAddressEdit}
+                            className="mt-2 text-[#6fc9cd] hover:text-[#2a9ca2] text-sm hover:underline text-end w-full"
+                          >
+                            Change Location?
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+
+                {/* Checkout Button */}
+                <button
+                  onClick={handleCheckout}
+                  className="btn  tracking-widest font-medium  mt-6 w-full px-6 py-3 bg-[#6fc9cd] hover:bg-[#2a9ca2] text-white  rounded-none"
+                >
+                  Proceed to Checkout
+                </button>
+              </section>
+            </div>
+          ) : (
+            <div className="flex justify-center items-center flex-col">
+              <h4 className=" text-2xl font-semibold">No Cart Available !!!</h4>
+              <Link href="/shop">
+                <button className="btn px-6 py-2 border-none rounded-none bg-white text-black shadow-md  tracking-widest font-normal mt-4">
+                  Continue Shopping
+                </button>
+              </Link>
+            </div>
+          )}
+        </>
       )}
 
       {currentStep === 2 && (
