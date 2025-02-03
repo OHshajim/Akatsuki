8f29e9ed5b11ac02f2c9fce0d8e7a7d45c6f145c
OHshajim <ajshajimmax@gmail.com>
2025-02-04 00:24:54 +0600

    admin nav add

---
+++ src/app/dashboard/[email]/page.tsx
@@ -0,0 +1,13 @@
+
+import DashboardNav from "@/Components/DashBoard/DashboardNav";
+
+const page = () => {
+  return (
+    <div className="min-h-screen">
+     
+      <DashboardNav />
+    </div>
+  );
+};
+
+export default page;
