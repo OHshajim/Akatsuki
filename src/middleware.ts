48d47bcf167bb3b78c228f491fc3cc3363659d1e
OHshajim <ajshajimmax@gmail.com>
2025-02-14 00:48:54 +0600

    project update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (request: NextRequest) => {
-  // const token = request.cookies.get("__Secure-next-auth.session-token");
-  const token = request.cookies.get("next-auth.session-token");
+  const token = request.cookies.get("__Secure-next-auth.session-token");
+  // const token = request.cookies.get("next-auth.session-token");
   const pathname = request?.nextUrl?.pathname;
 
   if (pathname.includes("api")) {
